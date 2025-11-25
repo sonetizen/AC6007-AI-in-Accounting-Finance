bc886fc41b46e2d2fe06fa8c8f777bbb9f7d0c7b
sonetizen <nuonsokhen123@gmail.com>
2025-11-25 03:16:27 +0000

    1

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
+#flask
+
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET","POST"])
 def index():
+    r = request.form.get("q")
     return(render_template("index.html"))
+
+@app.route("/main", methods=["GET","POST"])
+def main():
+    return(render_template("main.html"))
+
+@app.route("/dbs", methods=["GET","POST"])
+def dbs():
+    return(render_template("dbs.html"))
 
 if __name__=="__main__":
     app.run()
